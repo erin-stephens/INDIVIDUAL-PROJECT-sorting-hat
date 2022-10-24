b3d8c822d51a99905d828ebc779117064ccba744
Erin Stephens <erebstephens@gmail.com>
2022-10-23 21:04:49 -0500

    made tons of great progess, 2 issues to look into

--- main.js
+++ main.js
@@ -29,21 +29,51 @@ sortStartBtn.addEventListener('click', () => {
 const cardsOnDom = (array) => {
   let domString = "";
   for (const student of array) {
-    domString += `<div class="card" style="width: 18rem;">
+    domString += `<div class="card" style="width: 18rem;" id="studentCard">
     <div class="card-body">
       <h5 class="card-title">${student.name}</h5>
       <p class="card-text">${student.house}</p>
-      <a href="#" class="btn btn-primary" id="expel--${student.id}">Expel</a>
+      <button class="btn btn-primary" id="expel--${student.id}">Expel</button>
     </div>
   </div>`;
   }
-  renderToDom("#studentCard", domString);
+  renderToDom("#hogwartsCard", domString);
 };
 
+//const formShow = document.querySelector("#form");
+const createStudent = (e) => {
+  e.preventDefault();
+  
+  const houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+  
+  const newStudentObj = {
+    id: students.length + 1,
+    name: document.querySelector("#studentName").value,
+    house: houses[Math.floor(Math.random() * houses.length)]
+  }
+
+  students.push(newStudentObj);
+  cardsOnDom(students);
+  // form.reset();   This line is not working
+};
+
+form.addEventListener('submit', createStudent);
+
+const expelBtn = document.querySelector("#hogwartsCard");
+
+expelBtn.addEventListener('click', (e) => {
+  console.log("this clicks")
+
+  if (e.target.id.includes("expel")) {
+    const [, id] = e.target.id.split("--");
+    const index = students.findIndex(e => e.id === Number(id));
+    students.splice(index, 1); 
+    cardsOnDom(students);
+  }
+});
 
 const startApp = () => {
   cardsOnDom(students);
 }
 
 startApp();
-
